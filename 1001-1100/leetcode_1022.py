7e211ea6db9fb51ee624bdd4efa194bcbeb8a6b2
zzy <1584038779@qq.com>
2020-05-13 16:37:39 +0800

    add

---
+++ 1001-1100/leetcode_1022.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        self.sum = 0
+        self.getVal(root, 0)
+        return self.sum % (10 ** 9 + 7)
+
+    def getVal(self, root, val):
+        if root is None:
+            return
+        if root.left is None and root.right is None:
+            self.sum += val * 2 + root.val
+            return
+        self.getVal(root.left, val * 2 + root.val)
+        self.getVal(root.right, val * 2 + root.val)
