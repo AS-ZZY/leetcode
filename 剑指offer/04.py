789495fcd46dc59abf354510b336e66e471f4a9c
zzy <1584038779@qq.com>
2020-09-22 20:12:51 +0800

    add

---
+++ 剑指offer/04.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findNumberIn2DArray(self, matrix: list, target: int) -> bool:
+        if len(matrix) == 0:
+            return False
+        left, right = 0, len(matrix[0]) - 1
+        while left < len(matrix) and right >= 0:
+            a = matrix[left][right]
+            if a < target:
+                left += 1
+            elif a > target:
+                right -= 1
+            else:
+                return True
+        return False
